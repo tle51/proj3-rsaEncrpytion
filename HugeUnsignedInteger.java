9e45f86c3bc4a76b0b9165787047de43935e4ccb
tle51 <tle51@uic.edu>
2016-03-06 02:53:53 -0600

    Finished addition operation.

--- HugeUnsignedInteger.java
+++ HugeUnsignedInteger.java
@@ -3,12 +3,15 @@
   * Huge-Unsigned-Integer Class - Hold very large unsigned integer values and include operations
   */
 import java.io.*;
+import java.lang.*;
 
 public class HugeUnsignedInteger{
   private String value;
   private int[] arr;
   private int numDigit;
+  private int i, j;
   
+  //Constructor
   public HugeUnsignedInteger(String input){
     value = input;
     toArray();
@@ -17,7 +20,80 @@ public class HugeUnsignedInteger{
   //Covert value to array
   public void toArray(){
     numDigit = value.length();  //Count number of digits
+    arr = new int[numDigit];  //Dynamically allocate array space
     
+    //Store value to array
+    StringReader strRead = new StringReader(value);
+    try{
+      for(i=numDigit-1; i>=0; i--){
+        char c = (char) strRead.read();
+        arr[i] = Character.getNumericValue(c);
+      }
+    }
+    catch(IOException e){
+      System.err.println(e);
+    }
+  }
+  
+  //Addition Operation
+  public String addition(HugeUnsignedInteger value2){
+    String result = "";
+    int tempSize;
+    int tempCount;
+    //Check for the larger array size of the two operands
+    if(numDigit > value2.numDigit){
+      tempSize = numDigit;
+    }
+    else{
+      tempSize = value2.numDigit;
+    }
+    //Initialize temp array to zero
+    int[] tempArr = new int[tempSize+1];
+    for(i=0; i<tempSize+1; i++){
+      tempArr[i] = 0;
+    }
+    //Add two large int array together
+    j=0;
+    while(j < value2.numDigit || j < numDigit){
+      //If end of array1 -> copy rest of array2 contents
+      if(j >= numDigit){
+        tempArr[j] = tempArr[j] + value2.arr[j];
+      }
+      //If end of array2 -> copy rest of array1 contents
+      else if(j >= value2.numDigit){
+        tempArr[j] = tempArr[j] + arr[j];
+      }
+      else{
+        int tempAdd = arr[j] + value2.arr[j];
+        //Addition result is less than 10
+        if(tempAdd < 10){
+          tempArr[j] = tempAdd;
+        }
+        else{
+          tempArr[j] = tempAdd - 10;
+          tempArr[j+1] = 1;
+        }
+      }
+      j++;
+    }
+    //If there is an extra leading zero in the tempArr
+    if(tempArr[tempSize] == 0){
+      //Remove the extra space
+      tempCount = tempSize;
+    }
+    else{
+      tempCount = tempSize+1;
+    }
+    //Convert int array to string
+    for(i=tempCount-1; i>=0; i--){
+      result = result + Integer.toString(tempArr[i]);
+    }
+    
+    return result;
+  }
+  
+  //Subtraction Operation
+  public String subtraction(HugeUnsignedInteger value2){
     
   }
   
@@ -27,6 +103,9 @@ public class HugeUnsignedInteger{
   }
   public void printValue(){
     System.out.println(value);
+    for(i=0; i<numDigit; i++){
+      System.out.print(arr[i]);
+    }
   }
   //--------------------------------
 
--- Test.java
+++ Test.java
@@ -1,7 +1,8 @@
 public class Test{
   public static void main(String args[]){
-    HugeUnsignedInteger one = new HugeUnsignedInteger("1000009999999999999339393993");
-    System.out.println(one.getDigit());
-    one.printValue();
+    HugeUnsignedInteger one = new HugeUnsignedInteger("54932");
+    HugeUnsignedInteger two = new HugeUnsignedInteger("4000000");
+
+    System.out.println(one.addition(two));
   } 
 }
